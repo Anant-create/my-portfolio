04e96c36ca7e592d9fa2a067766f14ad71b19103
Anant Srivastava <anantsrivastava557@gmail.com>
2024-05-10 18:32:45 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Anant</h1>
+        <p className={styles.description}>
+        I'm versatile full stack developer passionate about crafting seamless user experiences, from intuitive interfaces to robust backend systems, driving business success.
+        </p>
+        <a href="mailto:anantsrivastava557@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
